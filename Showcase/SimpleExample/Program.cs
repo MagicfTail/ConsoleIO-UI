551238542ab2fd5f8cfba7e131171aec9e15abb4
Magic fTail <runecnielsen3132@gmail.com>
2023-10-18 18:24:06 +0200

    Restructure Showcase and add a simple exaple

---
+++ Showcase/SimpleExample/Program.cs
@@ -0,0 +1,20 @@
+﻿class Example
+{
+    static void Main()
+    {
+        Program program = new();
+
+        program.Start();
+    }
+
+    private class Program : ConsoleUI.ConsoleUI
+    {
+        public Program() : base() { }
+
+        public override void UserInputHandler(string input)
+        {
+            AddMessage(input, null);
+        }
+    }
+}
+
